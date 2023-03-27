5b4514e7e16e54684e109a82ee47ebdbd50da2de
Marcos José <marcosprofissional15@gmail.com>
2023-03-27 15:26:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastramento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Sistema de Cadastro--");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Informe o seu nome: ");
+            String nome = Console.ReadLine();
+
+            Console.WriteLine("Informe a sua idade: ");
+            sbyte idade = Convert.ToSByte(Console.ReadLine());
+
+            Console.WriteLine("Informe sua data de nascimento: ");
+            String dataNascimento = Console.ReadLine();
+
+
+            Console.WriteLine("\nNome = " + nome + "\nIdade = " + idade + "\nData de nascimento = " + dataNascimento);
+            Console.ReadLine();
+        }
+    }
+}

f652fa76c04104d7d7ae81cbd0e5ed7898cf46cd
Giuseppe Bruno <Giuseppebruno195@gmail.com>
2023-08-09 16:04:05 -0300

    Update ExcecoesPersonalizadas1

---
+++ Excecoes/ExcecoesPersonalizadas/Entities/Reservation.cs
@@ -0,0 +1,50 @@
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+namespace ExcecoesPersonalizadas.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+            + ", check-In: "
+            + CheckIn.ToString("dd/MM/yyyy")
+            + ", check-Out: "
+            + CheckOut.ToString("dd/MM/yyyy")
+            + ", "
+            + Duration()
+            + " nights";
+        }
+    }
+}
---
+++ Excecoes/ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ExcecoesPersonalizadas.Entities;
+//using System.Collections.Generic;
+//using System.Text;
+
+namespace ExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Check-In date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-Out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+            if (checkIn >= checkOut)
+            {
+                Console.WriteLine("Error in reservation: Check-Out date must be after check-In");
+            }
+
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine("\nEnter data to update the reservation:");
+                Console.Write("Check-In date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                Console.WriteLine(now);
+
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("1-Error in reservation: Reservation dates for update must be future dates");
+                }
+
+                else if (checkIn >= checkOut)
+                {
+                    Console.WriteLine("2-Error in reservation: Reservation dates for update must be future dates");
+
+                }
+
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+
+        }
+    }
+}
